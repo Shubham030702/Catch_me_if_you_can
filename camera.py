07a8ee0c1fa24cc8525b9f577bff240b63ae732f
Shubham030702 <dangwalshubham10@gmail.com>
2024-09-14 02:03:15 +0530

    new commit

--- camera.py
+++ camera.py
@@ -6,15 +6,14 @@ import math
 
 class VideoCamera(object):
     def __init__(self):
-        self.video=cv.VideoCapture(0)
-        self.video.set(3,1000)
-        self.video.set(4,1100)  
+        self.video = cv.VideoCapture(0)
+        self.video.set(3, 1000)
+        self.video.set(4, 1100)
         self.ctime = 0
-        self.ptime = 0  
+        self.ptime = 0
         self.game_time = time.time()
         self.start_time = time.time()
         self.position = self.get_random_position()
-        self.region = self.position[0]+30 , self.position[1]+30
         self.count = 0
         self.detector = pt.poseDetector()
 
@@ -22,37 +21,50 @@ class VideoCamera(object):
         self.video.release()
 
     def get_random_position(self):
-        self.x = random.randint(10,1000)
-        self.y = random.randint(10,900)
-        return self.x,self.y
-    
+        self.x = random.randint(10, 600)
+        self.y = random.randint(10, 400)
+        return self.x, self.y
+
     def get_frame(self):
         ret, frame = self.video.read()
-        frame = cv.flip(frame, 1)  # Flip the frame here to correct the mirror image
+        if not ret:
+            return None
+
+        frame = cv.flip(frame, 1)
         frame = self.detector.findPose(frame, draw=False)
         landms = self.detector.findLandmarks(frame, draw=False)
-        if len(landms)!=0 :
-            x,y=landms[20][1],landms[20][2]
-            x1,y1=landms[19][1],landms[19][2]
+
+        if len(landms) != 0:
+            x, y = landms[20][1], landms[20][2]
+            x1, y1 = landms[19][1], landms[19][2]
             distance = math.sqrt((self.position[0] - x) ** 2 + (self.position[1] - y) ** 2)
             distance2 = math.sqrt((self.position[0] - x1) ** 2 + (self.position[1] - y1) ** 2)
-            cv.circle(frame,(x,y),20,(255,0,255),-1)
-            cv.circle(frame,(x1,y1),20,(255,0,255),-1)
-            if (distance<30) | (distance2<30):
-                self.count+=1
-                self.position = self.get_random_position()    
-                region = self.position[0]+30 , self.position[1]+30
-                self.start_time = time.time()  
+            cv.circle(frame, (x, y), 20, (255, 0, 255), -1)
+            cv.circle(frame, (x1, y1), 20, (255, 0, 255), -1)
+
+            if distance < 30 or distance2 < 30:
+                self.count += 1
+                self.position = self.get_random_position()
+                self.start_time = time.time()
             if time.time() - self.start_time >= 3:
-                self.position = self.get_random_position()    
-                region = self.position[0]+30 , self.position[1]+30
-                self.start_time = time.time()  
+                self.position = self.get_random_position()
+                self.start_time = time.time()
+
         self.ctime = time.time()
-        fps = 1/(self.ctime-self.ptime)
+        fps = 1 / (self.ctime - self.ptime)
         self.ptime = self.ctime
-        cv.circle(frame,self.position,20,(255,255,255),-1)
-        cv.putText(frame,f'count:-{str(int(self.count))}',(500,70),cv.FONT_HERSHEY_SCRIPT_SIMPLEX,2,(255,0,255),3)
-        cv.putText(frame,f'FPS:-{str(int(fps))}',(30,70),cv.FONT_HERSHEY_SCRIPT_SIMPLEX,2,(255,0,255),3)
+
+        cv.circle(frame, self.position, 20, (255, 255, 255), -1)
+        cv.putText(frame, f'Count: {str(int(self.count))}', (500, 70), cv.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, (255, 0, 255), 3)
+        cv.putText(frame, f'FPS: {str(int(fps))}', (30, 70), cv.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, (255, 0, 255), 3)
+
+        if time.time() - self.game_time >= 40:
+            self.video.release()
+
         ret, jpeg = cv.imencode('.jpg', frame)
+        if not ret:
+            return None
         return jpeg.tobytes()
-    
+
+    def get_count(self):
+        return self.count
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask,render_template,Response
 from camera import VideoCamera
-
+import time
 # initializing flask
 
 app = Flask(__name__)
+
+camera = VideoCamera()
 
 # This is a route which will run browser on route '/'
 @app.route('/')
@@ -16,14 +18,24 @@ def vdo():
 
 def gen(camera):
     while True:
+        start_time = time.time()
         frame = camera.get_frame()
+        if frame is None:
+            continue
         yield(b'--frame\r\n'
               b'Content-type:image/jpeg\r\n\r\n' + frame +b'\r\n\r\n')
+        if time.time() - start_time >= 40:
+            break
 
 @app.route('/feed')
 def feed():
-    return Response(gen(VideoCamera()),
+    return Response(gen(camera),
                     mimetype='multipart/x-mixed-replace;boundary=frame')
+
+@app.route('/count')
+def final_count():
+    count = camera.get_count()
+    return f'Final Count: {count}'
 
 # Now how to run a html file
 
